8628b37e2496546429ff00c23272acfed798d657
Unknown <enricocaldini1234@gmail.com>
2018-11-14 17:31:21 +0100

    tempisom
    ok

---
+++ verifiva.swift
@@ -0,0 +1,14 @@
+//
+//  UIEsercizio.swift
+//  AppFranzi-Marchioro_Caldini
+//
+//  Created by Enrico Caldini on 03/10/18.
+//  Copyright © 2018 Enrico Caldini. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class verifiva {
+   
+}
